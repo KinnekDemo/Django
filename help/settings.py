b7112c103fa82329ddcb0c705d932bd2ac5c5402
Praful Rana <prafulrana@gmail.com>
2014-11-14 15:57:34 -0800

    Add right remote MYSQL host

--- help/settings.py
+++ help/settings.py
@@ -70,10 +70,15 @@ WSGI_APPLICATION = 'help.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'mysql.kinnekdemo.runnable.io',
+        'PORT':3306,
+        'NAME': 'help',
     }
 }
+
 
 #import dj_database_url
 #DATABASES['default'] = dj_database_url.config()
